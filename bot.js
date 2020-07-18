7adbd6f1d88c4a0a7ef79d0b956fb48bdaab958c
unknown <weiga666@gmail.com>
2020-07-18 12:58:43 +0800

    test commit

---
+++ bot.js
@@ -0,0 +1,17 @@
+    const {Client, RichEmbed, Discord} = require('discord.js') ;
+    const client = new Client ;
+    const settings = require('./settings.json') ;
+    
+
+
+    client.on('ready',()=>{
+        console.log(`${client.user.tag} 準備好上戰場惹！`) ;
+    }) ;
+
+    client.on('message',msg=>{
+        if(msg.content.startsWith(settings.prefix+"hey patrick")){
+            msg.channel.send("?") ;
+        }
+    }) ;
+
+    client.login(settings.token) ;
